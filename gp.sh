1b711d518e65a7dca0b2520167139226068c3d2c
vadic7wee2 <vadic7wee2@gmail.com>
2025-01-30 21:21:12 +0000

    test commit

--- gp.sh
+++ gp.sh
@@ -3,7 +3,7 @@ if [ ${#1} -eq 0 ]; then
   echo "Enter commit string."
   exit 1
 fi
-echo "Using commit string: $1"
+echo "Using1 commit string: $1"
 DATE=`date +%Y%m%d-%H%M`
 echo "Adding Files."
 git add . --all
